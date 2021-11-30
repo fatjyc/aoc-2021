5a906b213e28fa8a9abd318ee20afa49502f9ae9
jiong <fatjyc@gmail.com>
2021-11-30 11:55:21 +0800

    init

---
+++ generator.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+
+require "date"
+
+today = Date.today
+firstDay = Date.new(today.year, 11, 01)
+
+if today.month < 12
+  puts "This year’s puzzle is not yetopen!"
+  exit(1)
+end
+
+number = today - firstDay + 1
+
+num = number.to_i.to_s.rjust(2, '0')
+
+folder = "day#{num}"
+file = "day#{num}/day#{num}.rb"
+inputFile = "day#{num}/input"
+
+puts "create floder #{folder}"
+system("mkdir -p #{folder}")
+
+puts "create file"
+system("touch #{file}")
+
+
+fileContent = "#!/usr/bin/env ruby\n# -*- coding: UTF-8 -*-\n\ninputs = File.read(\"input\").split(\"\\n\")\n\n"
+
+File.open(file, 'w') do |file|
+  file.write(fileContent)
+end
+
+system("chmod +x #{file}")
+
+puts "create input file"
+
+system("touch #{inputFile}")
+
+puts "OK! Solve this puzzle!"
+puts "https://adventofcode.com/#{today.year}/day/#{number.to_i}"
